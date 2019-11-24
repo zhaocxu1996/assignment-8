7672049536004ebed3630960ee8b165d60cbfcc2
zhaocxu1996 <zhaocxu1996@github.com>
2019-11-24 15:17:50 -0500

    hw8

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,31 @@ let outputFilename = '[name].js';
 
 module.exports = {
     mode: 'production',
-    entry: './server.js',
+    entry: {
+        server: './server.js',
+      },
     output: {
         filename: outputFilename,
         path: outDirectory,
         publicPath: '/'
     },
-    devServer: {
-        contentBase: outDirectory
-    }
+    target: 'node',
+    node: {
+      // Need this when working with express, otherwise the build fails
+      __dirname: false,   // if you don't put this is, __dirname
+      __filename: false,  // and __filename return blank or /
+    },
+    module: {
+        rules: [
+          {
+            // Transpiles ES6-8 into ES5
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader"
+            }
+          }
+        ]
+      },    
+
 };
